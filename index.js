f6d84599efc8a24b4ac646f157216b2ce43f439a
Krushna Nagare <contact.nagare@gmail.com>
2023-10-08 20:57:55 +0530

    Changed cors to localhost

--- index.js
+++ index.js
@@ -16,8 +16,12 @@ db.on('error' , (error)=>{console.log(error);});
 db.once('open' , ()=>{console.log("Conneced to MongoDB Atlas Successfully");});
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors({ origin: '*', credentials: true }));
-//set up route
+const corsOptions ={
+    origin:'http://localhost:3000', 
+    credentials:true,            //access-control-allow-credentials:true
+    optionSuccessStatus:200
+}
+app.use(cors(corsOptions));//set up route
 
 app.use("/auth" , require('./routes/userRoutes'));
 app.use('/customer' , require('./routes/customerRoutes'))
